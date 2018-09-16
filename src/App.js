2dd15c8f756dde506eef7008e2bdcada505b234c
Pedro R. Jiménez <ptpedroj@gmail.com>
2018-09-16 01:22:59 -0500

    More Flow Config Refinements and Flow Fixes to Code
    Added script commands to package.json
    Updated settings.json to eliminate false validation errors.
    Fixed App.js and index.js to pass Flow check.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
+//@flow
+import React, { Component } from "react";
+import "./App.css";
 
-class App extends Component {
+class App extends Component<{}> {
   render() {
     return (
       <div className="App">
-          <h1>Hi, I'm a React App!</h1>
+        <h1>Hi, I'm a React App!</h1>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//@flow
+import * as React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const root = document.getElementById("root");
+if (root) {
+  ReactDOM.render(<App />, root);
+  registerServiceWorker();
+}
